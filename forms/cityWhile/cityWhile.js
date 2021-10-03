d7658f44ef5e0dbe5d620cf27c61a732db3d4ecb
HaleyGilbertson <hag79621@creighton.edu>
2021-10-03 13:11:14 -0500

    Initial commit

---
+++ forms/cityWhile/cityWhile.js
@@ -0,0 +1,17 @@
+
+let wantToContinue = true
+let city = []
+let enterCity = ""
+
+while (wantToContinue) {
+  let enterCity = prompt("Enter a city:");
+  city.push(enterCity)
+  wantToContinue = confirm("Do you want to enter another city")
+  }
+  
+  i = 0
+  
+  while (i < city.length) {
+    console.log(city[i].toLowerCase())
+    i = + 1
+    }
